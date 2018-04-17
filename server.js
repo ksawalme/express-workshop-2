2e5e0c6b0d50587e79648b5e57a46fc28edae6bc
ksawalme <ksawalme@gmail.com>
2018-04-17 22:01:02 +0100

    Add Template

--- server.js
+++ server.js
@@ -15,14 +15,11 @@ app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
   res.render("index");
+
 });
 
-app.get("/admin", (req, res) => {
-  res.render("admin");
-});
-app.get("/my-cv", (req, res) => {
-  res.render("my-cv");
-});
+
+
 // The extensions 'html' allows us to serve file without adding .html at the end 
 // i.e /my-cv will server /my-cv.html
 app.use(express.static("public", {'extensions': ['html']}));
@@ -33,7 +30,3 @@ app.listen(process.env.PORT || 3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
 
-app.get('/contact',  function (req, res)
-{
-  res.end("call me at number 07492936847")
-});
